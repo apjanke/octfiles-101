635839a187042f22b9812386a63055d3c11eab88
Andrew Janke <floss@apjanke.net>
2019-01-28 15:24:51 -0500

    Add notes on octave_idx_type

--- doc-src/src/ex03_inputs_and_error_checking.cc
+++ doc-src/src/ex03_inputs_and_error_checking.cc
@@ -5,17 +5,17 @@
 DEFUN_DLD (ex03_inputs_and_error_checking, args, nargout,
   "Inputs and error checking")
 {
-  int nargin = args.length ();
+  octave_idx_type nargin = args.length ();
   if (nargin != 2) {
-  	error ("Need exactly 2 argins, got %d", nargin);
+  	error ("Need exactly 2 argins, got %ld", (long) nargin);
   }
   NDArray x = args(0).array_value ();
   NDArray y = args(1).array_value ();
 
   NDArray my_diff = x - y;
   // This "x * y" results in a compilation error. I wonder why?
-  NDArray my_product = x * y;
-  //NDArray my_product (0.0);
+  //NDArray my_product = x * y;
+  NDArray my_product (0.0);
   octave_value_list varargout;
   varargout(0) = my_diff;
   varargout(1) = my_product;
