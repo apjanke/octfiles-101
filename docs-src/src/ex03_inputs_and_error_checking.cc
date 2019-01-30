fe03660a62b9572255bda75785b775b15569236b
Andrew Janke <floss@apjanke.net>
2019-01-29 19:23:44 -0500

    Some more prose

--- docs-src/src/ex03_inputs_and_error_checking.cc
+++ docs-src/src/ex03_inputs_and_error_checking.cc
@@ -12,7 +12,7 @@ DEFUN_DLD (ex03_inputs_and_error_checking, args, nargout,
   NDArray x = args(0).array_value ();
   NDArray y = args(1).array_value ();
   if (x.any_element_is_nan ()) {
-    warning ("");
+    warning ("NaNs present in input");
   }
 
   return ovl (y, x);
